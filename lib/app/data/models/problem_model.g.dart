ba5174990debd4cb21faa17ad12afd0338708607
Sabiha <ssabiha104@gmail.com>
2026-05-23 00:24:16 +0600

    add data model for problems

---
+++ lib/app/data/models/problem_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'problem_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Problem _$ProblemFromJson(Map<String, dynamic> json) => Problem(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      slug: json['slug'] as String,
+      statement: json['statement'] as String?,
+      difficulty: json['difficulty'] as String,
+      rating: (json['rating'] as num).toInt(),
+      topicId: json['topic_id'] as String?,
+    );
+
+Map<String, dynamic> _$ProblemToJson(Problem instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'slug': instance.slug,
+      'statement': instance.statement,
+      'difficulty': instance.difficulty,
+      'rating': instance.rating,
+      'topic_id': instance.topicId,
+    };
