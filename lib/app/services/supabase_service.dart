649ffd86bdda2aca42496f8837bd5eae3f7489b7
Sabiha <ssabiha104@gmail.com>
2026-05-23 00:13:14 +0600

    Add Supabase service layer

---
+++ lib/app/services/supabase_service.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseService extends GetxService {
+  late final SupabaseClient client;
+
+  Future<SupabaseService> init() async {
+    client = Supabase.instance.client;
+    return this;
+  }
+
+  User? get currentUser => client.auth.currentUser;
+  Session? get currentSession => client.auth.currentSession;
+  Stream<AuthState> get authStateChanges => client.auth.onAuthStateChange;
+}
