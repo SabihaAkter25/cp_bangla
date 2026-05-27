00954f2e44c2bb7e5b9e9c7b04f1cb1fbd10b29b
Sabiha <ssabiha104@gmail.com>
2026-05-27 13:51:20 +0600

    updated problem controller

--- lib/app/modules/problems/controllers/problem_controller.dart
+++ lib/app/modules/problems/controllers/problem_controller.dart
@@ -11,16 +11,48 @@ class ProblemController extends GetxController {
   final searchQuery = ''.obs;
   final selectedTopicId = ''.obs;
   final selectedDifficulty = ''.obs;
+  final solvedCount = 0.obs;
+  final isCurrentProblemSolved = false.obs;
 
   @override
   void onInit() {
     super.onInit();
     fetchProblems();
+    fetchSolvedCount();
 
     // Setup listeners
     debounce(searchQuery, (_) => _filterProblems(), time: const Duration(milliseconds: 500));
     ever(selectedTopicId, (_) => _filterProblems());
     ever(selectedDifficulty, (_) => _filterProblems());
+  }
+
+  Future<void> fetchSolvedCount() async {
+    solvedCount.value = await _problemService.getSolvedProblemsCount();
+  }
+
+  Future<void> checkProblemStatus(String problemId) async {
+    isCurrentProblemSolved.value = await _problemService.hasSolved(problemId);
+  }
+
+  Future<bool> submitAnswer(Problem problem, String answer) async {
+    try {
+      isLoading.value = true;
+      final bool isCorrect = problem.correctAnswer?.trim().toLowerCase() == answer.trim().toLowerCase();
+      
+      await _problemService.submitAnswer(problem.id, answer, isCorrect);
+      
+      if (isCorrect) {
+        await fetchSolvedCount();
+        isCurrentProblemSolved.value = true;
+      }
+      
+      return isCorrect;
+    } catch (e) {
+      Get.snackbar('Error', e.toString());
+      return false;
+    } finally {
+      isLoading.value = false;
+    }
   }
 
   void searchProblems(String query) {
@@ -69,7 +101,7 @@ class ProblemController extends GetxController {
     }
   }
 
-  Future<void> createProblem(String title, String slug, String statement, String difficulty, int rating, String topicId) async {
+  Future<void> createProblem(String title, String slug, String statement, String difficulty, int rating, String topicId, String correctAnswer) async {
     try {
       isLoading.value = true;
       await _problemService.createProblem({
@@ -79,6 +111,7 @@ class ProblemController extends GetxController {
         'difficulty': difficulty,
         'rating': rating,
         'topic_id': topicId,
+        'correct_answer': correctAnswer,
       });
       await fetchProblems();
       Get.back();
