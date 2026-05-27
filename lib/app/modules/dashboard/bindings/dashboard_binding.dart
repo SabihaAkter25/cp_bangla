e6b4e0cf9a9764a968137461207905dd0f3c82df
Sabiha <ssabiha104@gmail.com>
2026-05-27 13:50:09 +0600

    registered problem detail page

--- lib/app/modules/dashboard/bindings/dashboard_binding.dart
+++ lib/app/modules/dashboard/bindings/dashboard_binding.dart
@@ -5,12 +5,14 @@ import '../../problems/controllers/problem_controller.dart';
 import '../../problems/services/problem_service.dart';
 import '../../topics/controllers/topic_controller.dart';
 import '../../topics/services/topic_service.dart';
+import '../../profile/controllers/profile_controller.dart';
 import '../controllers/dashboard_controller.dart';
 
 class  DashboardBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<DashboardController>(() => DashboardController());
+    Get.lazyPut<ProfileController>(() => ProfileController());
     
     // Articles
     Get.lazyPut<ArticleService>(() => ArticleService());
